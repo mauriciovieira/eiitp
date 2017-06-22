06ddc9fa0d9bc69112d0c07332a202e0922f2eea
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-06-22 10:52:20 -0300

    Fix aliases and add a few more

--- playbooks/templates/shell/aliases.zsh
+++ playbooks/templates/shell/aliases.zsh
@@ -1,2 +1,11 @@
-cd_eiitp='cd ~/Repositories/github.com/mauriciovieira/eiitp/'
-run_eiitp='cd_eiitp && make go'
+# Projects
+
+## Apinf
+### https://github.com/apinf/platform
+alias cd_apinf='cd ~/Repositories/github.com/apinf'
+alias cd_platform='cd ~/Repositories/github.com/apinf/platform'
+
+## Mauricio Vieira
+### https://github.com/mauriciovieira/eiitp
+alias cd_eiitp='cd ~/Repositories/github.com/mauriciovieira/eiitp/'
+alias run_eiitp='cd_eiitp && make go'
