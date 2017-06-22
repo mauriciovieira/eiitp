da664a646764a488ddcbe81e154b7867cc7a0a5f
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-06-22 18:01:40 -0300

    Add some aliases

--- playbooks/templates/shell/aliases.zsh
+++ playbooks/templates/shell/aliases.zsh
@@ -1,11 +1,13 @@
 # Projects
 
 ## Apinf
-### https://github.com/apinf/platform
+### https://github.com/apinf/
 alias cd_apinf='cd ~/Repositories/github.com/apinf'
 alias cd_platform='cd ~/Repositories/github.com/apinf/platform'
 
 ## Mauricio Vieira
-### https://github.com/mauriciovieira/eiitp
+### https://github.com/mauriciovieira/
+alias cd_mauriciovieira='cd ~/Repositories/github.com/mauriciovieira'
 alias cd_eiitp='cd ~/Repositories/github.com/mauriciovieira/eiitp/'
 alias run_eiitp='cd_eiitp && make go'
+alias cd_mv_dot_net='cd ~/Repositories/github.com/mauriciovieira/mauriciovieira.net'
