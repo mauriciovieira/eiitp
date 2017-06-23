59ab95bc59379f73a6009e716fb884dc7e55e60b
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-06-22 21:49:46 -0300

    Add git aliases

--- playbooks/templates/shell/aliases.zsh
+++ playbooks/templates/shell/aliases.zsh
@@ -11,3 +11,43 @@ alias cd_mauriciovieira='cd ~/Repositories/github.com/mauriciovieira'
 alias cd_eiitp='cd ~/Repositories/github.com/mauriciovieira/eiitp/'
 alias run_eiitp='cd_eiitp && make go'
 alias cd_mv_dot_net='cd ~/Repositories/github.com/mauriciovieira/mauriciovieira.net'
+alias ll='ls -alF'
+
+# Git
+
+## Checkout
+
+alias gco='git checkout'
+alias gbc='git checkout -b'
+
+## Add
+
+alias ga='git add -A'
+
+## Commit
+
+alias gc='git commit'
+
+## Diff
+
+alias gd='git diff'
+
+## Fetch/Pull
+
+alias gf='git fetch'
+alias gfm='git pull'
+alias gfr='git pull --rebase'
+
+## Push
+
+alias gpoh='git push origin HEAD'
+alias gps='git push'
+alias gpf='git push --force'
+
+## Status
+
+alias gs='git status'
+
+## Remote
+
+alias gRl='git remote -v'
